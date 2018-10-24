152b9c6e6a21c6de3a12ecb7bae0784455faace3
daneelsheleg <42346177+daneelsheleg@users.noreply.github.com>
2018-10-24 21:59:20 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,81 @@
+/*var userPassword = prompt('введите пароль');
+const originPassword = '1111';
+
+if (userPassword === originPassword) {
+    alert('ok');
+} else{
+    alert('not')
+}*/
+
+
+/*var one = prompt('one');
+var two = prompt('two');
+alert(parseInt(one)+parseInt(two));
+
+var one = +prompt('one');
+var two = +prompt('two');
+alert(one+two)*/
+
+/*let arr = [1, 2, 3];
+for(let i=0; i < arr.length; i+=1){
+    console.log(arr[i]+1);
+}*/
+
+
+
+
+
+/*
+
+var c = prompt('введите массив').split(' ');
+var sum = 1;
+
+for (var i = 0; i < c.length; i = i + 1){
+    sum = c[i] * sum;
+}
+
+console.log(sum)
+
+*/
+
+
+
+
+/*
+var a = prompt ('введите массив').split(' ');
+var num = 0;
+    for ( var i = 0; i < a.length; i = i + 1){
+        num = a[i];
+        if ( num > 10) {
+            console.log(num)
+        }
+    }
+    */
+
+
+
+
+/*
+var mass = prompt('Массив').split(' ');
+    for ( var i = 0; i < mass.length; i++){
+
+        if (mass[i] % 2 == 0){
+            console.log(mass[i]*mass[i])
+        }
+        else{
+            console.log(mass[i])
+        }
+    }
+*/
+
+/*
+var mass = prompt('массив').split(' ');
+    for (var i = 0; i < mass.length; i++){
+        num = mass[i];
+            if( num > 10 && num < 20){
+                console.log(num)
+            }
+    }
+    */
+
+
